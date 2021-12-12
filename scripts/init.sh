bafbb72db1867951cbcdafe0ece8cf5313822ce1
BuildTools <unconfigured@null.spigotmc.org>
2021-12-12 02:57:41 +0000

    Add tmux conf

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,8 @@
+echo "Installing vimplug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Symlinking vimrc"
 ln -sr ../dotfiles/.vimrc ~/.vimrc
 
+echo "Symlinking tmux.conf"
+ln -s ../dotfiles/.tmux.conf ~/.tmux.conf
